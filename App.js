058483f6f00cc30dc2324615c96dfd432ee8b8ce
João Rodrigo Prieto <jrp2033@gmail.com>
2022-03-05 11:55:33 -0300

    base

---
+++ App.js
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Hello World</Text>
+    </View>
+  )
+}
